3c3aee40a91012a2d120ca3e336da38625f9b278
LQ <liqiangeastsun@163.com>
2021-02-23 17:25:51 +0800

    no message

---
+++ DataStruct/BinaryTree/BTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStruct.BinaryTree
+{
+    public class BTNode<T>
+    {
+        private BTNode<T> _leftChild;
+        private BTNode<T> _rightChild;
+
+        private T _value;
+
+        public BTNode<T> LeftChild
+        {
+            get { return _leftChild; }
+            set { _leftChild = value; }
+        }
+
+        public BTNode<T> RightChild
+        {
+            get { return _rightChild; }
+            set { _rightChild = value; }
+        }
+
+        public T Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+    }
+
+    class BTree<T>
+    {
+        private BTNode<T> _root;
+
+        public BTree()
+        {
+
+        }
+
+        public BTNode<T> Root
+        {
+            get { return _root; }
+        }
+
+        public bool Empty()
+        {
+            return null == Root;
+        }
+
+        public virtual bool Insert(T t)
+        {
+            return true;
+        }
+
+        public virtual bool Remove(T t)
+        {
+            return false;
+        }
+
+        public void Release()
+        {
+
+        }
+
+    }
+}
